7e3dac586a7285ac8b610e979292da9659101758
Shiham Samsudeen <shihams247@gmail.com>
2024-04-19 11:30:29 +0530

    Moved Files to RMIDemo2023

--- src/ImplmyFirst.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.RemoteException;
-
-public class ImplmyFirst implements myFirst {
-    @Override
-    public int calc(int a, int b) throws RemoteException {
-        return (a + b);
-    }
-}
--- src/myFirst.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface myFirst extends Remote {
-    public int calc(int a, int b) throws RemoteException;
-}
